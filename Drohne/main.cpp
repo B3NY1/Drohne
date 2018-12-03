1dfc6c047f3609aee2e51a0347f6cdfa9d5cbf91
felixp98 <felix.preuschoff@web.de>
2018-12-03 15:42:40 +0100

    added PointList class

--- Drohne/main.cpp
+++ Drohne/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include "Point.h"
+#include "PointList.h"
 
 int main() {
-	Point* p = new Point("deine mum",20,10);
-	p->Print();
+	Point* p1 = new Point("dhbw saftladen",20,10);
+	Point* p2 = new Point("miau", 30, -29);
 
+	auto* pList = new PointList();
+	pList->add(*p1);
+	pList->add(*p2);
+	pList->print();
 
     return 0;
 }
